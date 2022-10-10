d24b127215aeee7eb0b3cb86e51b4ad478071a91
csh4430 <snghun6889@gmail.com>
2022-10-11 00:49:15 +0900

    s

--- Assets/03.Scripts/Unit/Enemy/03.Mino/SkillDash.cs
+++ Assets/03.Scripts/Unit/Enemy/03.Mino/SkillDash.cs
@@ -10,6 +10,7 @@ public class SkillDash : EnemySkill
     public override void DoAttack(UnitMove unit, Action callback = null)
     {
         int damage = MapController.Instance.MapNum[unit.GamePos.y, unit.GamePos.x];
+        float range = GameManager.Instance.Size / 2f * MapController.Instance.Distance;
         Vector3 direction = ((Vector2)Define.PlayerMove.GamePos - unit.GamePos).normalized;
         Vector2Int dir = new Vector2Int(Mathf.CeilToInt(direction.x), Mathf.CeilToInt(direction.y));
         direction = new Vector3(dir.x, 0, dir.y);
@@ -18,9 +19,9 @@ public class SkillDash : EnemySkill
         for(int i = 1; i <= GameManager.Instance.Size; i++)
         {
             Vector3 temp = unit.WorldPos + direction * i * MapController.Instance.Distance;
-            if (temp.x < -3 || temp.x > 3 || temp.z < -3 || temp.z > 3)
+            if (temp.x <= -range || temp.x >= range || temp.z <= -range || temp.z >= range)
                 break;
-            seq.Append(unit.transform.DOLocalMove(temp, 0.3f));
+            seq.Append(unit.transform.DOLocalMove(temp, 0.1f));
             seq.AppendCallback(() =>
             {
                 unit.WorldPos = unit.transform.localPosition;
