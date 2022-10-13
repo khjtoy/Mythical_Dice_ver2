48ba96710cb3c39cc700ed6056885c36ac58d901
ehddbs <ehddbs295@gmail.com>
2022-10-13 23:29:12 +0900

    세이렌 패턴 시간 수정

--- Assets/03.Scripts/SO/StageSO.cs
+++ Assets/03.Scripts/SO/StageSO.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "SO/StageStatus")]
+[Obsolete("이거 이제 안씀")]
 public class StageSO : ScriptableObject
 {
+    
      public List<Stage> stages = new List<Stage>();
 }
--- Assets/03.Scripts/UI/Stage/StageContoller.cs
+++ Assets/03.Scripts/UI/Stage/StageContoller.cs
@@ -5,21 +5,11 @@ using UnityEngine.UI;
 
 public class StageContoller : MonoBehaviour
 {
-    [SerializeField]
-    StageSO stageSO = null;
+
 
     private void Awake()
     {
         
     }
 
-    private void CreateLevelUI()
-    {
-        for(int i =0;i< stageSO.stages.Count;i++)
-        {
-            //GameObject levelPanel;
-            
-            //levelUI.rectTransform.anchoredPosition = stageSO.stages[i].pos;
-        }
-    }
 }
